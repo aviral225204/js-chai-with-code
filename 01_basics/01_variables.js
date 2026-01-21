0444c1012ffd45545947760057cdda28ac32fff1
aviral225204 <aviralverma4850@gmail.com>
2026-01-21 07:16:49 +0000

    add variables and datatypes basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId =14453
+let accountEmail="aviral2252@gogle.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+
+
+// accountId =2  -- comment out // this is not allowed
+
+accountEmail ="av@av.com"
+accountPassword ="55244"
+accountCity ="Delhi"
+let accountState;
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+
+because of issue in block scope and Functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3) // we are using Node js ,not browser ingune soo this is not run
+
+console.log(3
+    +
+    3); //code Readability should be high
+
+console.log("Hitesh");   
+
+/*
+Documentation Refer 
+Origanal-ECMA tc39
+and Local MDN REFERENCE
+*/
+
+let name ="Aviral"
+let age =18
+let isLoggedIn =false
+
+//         DATA TYPES
+
+//number =>2 to power 53
+// bigint 
+//string=>"Aviral"
+// boolean=>true/false
+//null=>standalone value ->ham difine kar dete hai so undefined not show
+//Undefined=>not defined
+//symbol=>unique
+
+
+//   Object
+
+console.log(typeof "age");
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
+
