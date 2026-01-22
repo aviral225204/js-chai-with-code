f1a81540d91aaca8ea6150b8a925c82ede21c18b
aviral225204 <aviralverma4850@gmail.com>
2026-01-22 17:09:52 +0000

    Conversion Of Data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,37 @@
+let score ="Aviral"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+
+//"33" =>33
+//"33abc" =>NaN (NOT A Number)
+//True ==>1;  False==>0
+
+let isLoggedIn="Aviral"
+//console.log(typeof isLoggedIn);
+
+         // Conversion
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 ==>true; 0==>false
+//"" ==>False
+//"Aviral" ==>True
+
+let someNumber =33
+//console.log(typeof someNumber);
+
+
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
