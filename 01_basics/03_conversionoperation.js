4e13b93ead19b5202e439425082c11cda81cab71
aviral225204 <aviralverma4850@gmail.com>
2026-01-22 17:37:57 +0000

    conversion part2

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -31,6 +31,44 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+// *************************************OPERATIONS***********************************************
+let value =3
+let negvalue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1="hello"
+let str2="Aviral"
+
+let str3 =str1 +str2
+console.log(str);
+
+
+//PREFIX AND POSTFIX EXAMPLE
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
 
 
 
