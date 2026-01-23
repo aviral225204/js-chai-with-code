147cca355c9e440b63ed441bb0784733aaf184d0
aviral225204 <aviralverma4850@gmail.com>
2026-01-23 07:41:46 +0000

    comparsion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,39 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==2);
+console.log(2!=1);
+
+// Different type of datatype compare
+console.log("2">1);
+console.log("02">1);
+
+
+//compare NUll
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+//compare Undefined
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//  Compare strictly (Number+Datatype check)  ===
+
+console.log("2"===2);
+
+// Strictly  
+console.log(2===2);
+
+
+
+
+
+
+
+
